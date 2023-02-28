714aaad797c1d397faf7152a9e561d0127809e3d
RAJADURAIRAGUNATHAN <124970669+RAJADURAIRAGUNATHAN@users.noreply.github.com>
2023-03-01 00:47:08 +0530

    Add files via upload

---
+++ day 3 task.js
@@ -0,0 +1,23 @@
+var request=new XMLHttpRequest();
+request.open("GET","https://restcountries.com/v2/all");
+request.send();
+request.onload=function(){
+    var result=JSON.parse(request.response);
+    console.log(result);
+
+
+    for(var i=0;i<result.length;i++){
+        console.log(result[i].flags);
+ 
+    }
+}
+var request=new XMLHttpRequest();
+request.open("GET","https://restcountries.com/v2/all");
+request.send();
+request.onload=function(){
+    var result=JSON.parse(request.response);
+    console.log(result);
+    for(var i=0;i<result.length;i++){
+        console.log(result[i].region,result[i].sub-region,result[i].population);
+    }
+}
